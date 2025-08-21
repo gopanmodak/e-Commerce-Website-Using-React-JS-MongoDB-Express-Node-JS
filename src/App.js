a62b3182bbcb28f0391f3408eb68c3bc63eb6205
gopanmodak <gopanhridoy@gmail.com>
2025-08-21 15:02:41 +0600

    change

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ function App() {
           <Route path="/mens" element={<ShopCategory category="men" />} />
           <Route path="/womens" element={<ShopCategory category="women" />} />
           <Route path="/kids" element={<ShopCategory category="kid" />} />
-          <Route path="/product" element={<Product />} >
-          <Route path=":productId" element={<Product />} />
-          </Route>
+          <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<LoginSignUp />} />
         
