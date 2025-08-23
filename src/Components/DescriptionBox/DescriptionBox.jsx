8e8e00f2a78a7c17c9d131ed92530fadc00f115d
gopanmodak <gopanhridoy@gmail.com>
2025-08-23 23:52:26 +0600

    Added Breadcrump & Description box

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescroptionBox.css'
+ const DescriptionBox = () => {
+  return (
+    <div className='descroptionbox'>
+   <div className="descriptionbox-navigator">
+    <div className="descriptionbox-nav-box">Description</div>
+    <div className="descriptionbox-nav-fade">Reviews(350)</div>
+   </div>
+  
+<div className="descriptionbox-description">
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias dignissimos ipsam officiis obcaecati ratione eum magni illo laboriosam sed eos, asperiores voluptatem voluptate, officia corrupti debitis nesciunt quaerat error vero?</p>
+</div>
+    </div>
+  )
+}
+
+export default DescriptionBox
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrump from '../Components/Breadcrumps/Breadcrump';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
  const Product = () => {
+  const{all_product}=useContext(ShopContext);
+
+  const {productId}=useParams();
+  const product=all_product.find((e)=>e.id===Number(productId));
+
   return (
-    <div></div>
+    <div>
+    <Breadcrump product={product}/>
+    <ProductDisplay product={product}/>
+    </div>
   )
 }
 export default Product
